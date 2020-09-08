5b7b57186dde1cdd6ee4c11de4735aecc3743af7
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-09-08 18:18:31 +0200

    Fix tflint errors
    Fixes "Warning: List items should be accessed using square brackets (terraform_deprecated_index)"

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "cloudflare_zones" "main" {
 
 data "cloudflare_waf_packages" "main" {
   for_each = toset(var.rulesets.*.name)
-  zone_id  = data.cloudflare_zones.main.zones.0.id
+  zone_id  = data.cloudflare_zones.main.zones[0].id
   filter {
     name = each.key
   }
@@ -16,8 +16,8 @@ data "cloudflare_waf_packages" "main" {
 
 data "cloudflare_waf_groups" "main" {
   for_each   = { for g in local.rule_groups : g.name => g }
-  zone_id    = data.cloudflare_zones.main.zones.0.id
-  package_id = data.cloudflare_waf_packages.main[each.value.package].packages.0.id
+  zone_id    = data.cloudflare_zones.main.zones[0].id
+  package_id = data.cloudflare_waf_packages.main[each.value.package].packages[0].id
   filter {
     name = each.key
   }
@@ -25,22 +25,22 @@ data "cloudflare_waf_groups" "main" {
 
 resource "cloudflare_waf_package" "main" {
   for_each    = { for rs in var.rulesets : rs.name => rs if rs.mode != "" }
-  package_id  = data.cloudflare_waf_packages.main[each.key].packages.0.id
-  zone_id     = data.cloudflare_zones.main.zones.0.id
+  package_id  = data.cloudflare_waf_packages.main[each.key].packages[0].id
+  zone_id     = data.cloudflare_zones.main.zones[0].id
   sensitivity = each.value.sensitivity
   action_mode = each.value.mode
 }
 
 resource "cloudflare_waf_group" "main" {
   for_each = { for g in local.rule_groups : g.name => g }
-  group_id = data.cloudflare_waf_groups.main[each.key].groups.0.id
-  zone_id  = data.cloudflare_zones.main.zones.0.id
+  group_id = data.cloudflare_waf_groups.main[each.key].groups[0].id
+  zone_id  = data.cloudflare_zones.main.zones[0].id
   mode     = each.value.mode
 }
 
 resource "cloudflare_waf_rule" "main" {
   for_each = { for r in local.rules : r.id => r }
   rule_id  = each.key
-  zone_id  = data.cloudflare_zones.main.zones.0.id
+  zone_id  = data.cloudflare_zones.main.zones[0].id
   mode     = each.value.mode
 }
